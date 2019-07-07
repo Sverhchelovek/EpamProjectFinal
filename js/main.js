51ba8c60af123ddac33289f1334f8e58aa2c6ec0
Bogdan Shon <bogdan5321@gmail.com>
2019-07-07 21:52:49 +0300

    started header

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getRandomInt(min, max){
+ 	const number = Math.floor(Math.random() * (max - min + 1)) + min;
+ 	return number;
+ 	console.log(number);
+}
+
+let randomNumber = getRandomInt(1, 100);
+
+
+function myDesicion(){
+	let myNumber = prompt('Choose your number');
+	if (myNumber === randomNumber) {
+		console.log('Congratulations! U won');
+	} else {
+		let restNumber = myNumber - 
+	}
+}
+
+myDesicion();
+
+a5	`	aw23w2`
