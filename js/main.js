71896d31896bb04168fee748d00eb2bdc99713cd
Bogdan Shon <bogdan5321@gmail.com>
2019-07-22 04:20:22 +0300

    sa

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// let map;
+// function initMap(){
+// 	map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 50.465019, lng: 30.514980},
+//     zoom: 14
+//   });
+// }
