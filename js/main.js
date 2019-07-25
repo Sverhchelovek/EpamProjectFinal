21ed1fb747869547a489f7497d6e7c34ccf0cbed
Bogdan Shon <bogdan5321@gmail.com>
2019-07-25 15:56:47 +0300

    in progress

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-// let map;
-// function initMap(){
-// 	map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 50.465019, lng: 30.514980},
-//     zoom: 14
-//   });
-// }
+function initMap() {
+  let myLocation = {lat: 50.465019, lng: 30.514980};
+
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: myLocation
+  });
+
+  let marker = new google.maps.Marker({
+    position:myLocation,
+    map: map,
+    title: 'PT'
+  });
+}
