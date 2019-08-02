a02b4884c46134bea014b2fdbedce9c3cae499f3
Bogdan Shon <bogdan5321@gmail.com>
2019-08-03 00:40:02 +0300

    filtration done

--- js/showCards.js
+++ js/showCards.js
@@ -1,347 +1,285 @@
-let houses = [
-       {
-      	"photo": "url('../img/apartment1.jpg')", 
-        "price": "34800",
-        "location": "Дмитриевская ул. 96/98 ",  
-        "district": "Шевченковский р-он",
-        "code": "123", 
-        "room":"1", 
-        "square": "123",  
-        "floor":"1",
-        },
-       { 
-      	"photo": "url(../img/apartment1.jpg)", 
-        "price": "120000",
-        "location": "Дмитриевская ул. 96/98 ",  
-        "district": "Шевченковский р-он",
-        "code": "120000", 
-        "room":"1", 
-        "square": "123",  
-        "floor":"1",
-        },
-       { 
-      	"photo": "url(../img/apartment1.jpg)", 
-        "price": "70000",
-        "location": "Дмитриевская ул. 96/98", 
-        "district": "Шевченковский р-он", 
-        "code": "123", 
-        "room":"1", 
-        "square": "123",  
-        "floor":"1",
-        }
-];
 
-let apartments = [
-       {
+
+let data = [
+		
+	[
+		{
+			"photo": "url('../img/apartment1.jpg')", 
+		"price": "34800",
+		"location": "Дмитриевская ул. 96/98 ",  
+		"district": "Шевченковский",
+		"code": "123", 
+		"room":"1", 
+		"square": "123",  
+		"floor":"1",
+		},
+		{ 
+			"photo": "url(../img/apartment1.jpg)", 
+		"price": "120000",
+		"location": "Дмитриевская ул. 96/98 ",  
+		"district": "Шевченковский",
+		"code": "120000", 
+		"room":"1", 
+		"square": "123",  
+		"floor":"1",
+		},
+		{ 
+			"photo": "url(../img/apartment1.jpg)", 
+		"price": "70000",
+		"location": "Дмитриевская ул. 96/98", 
+		"district": "Шевченковский", 
+		"code": "123", 
+		"room":"1", 
+		"square": "123",  
+		"floor":"1",
+		}
+	],
+
+	[
+ 		{
       	"photo": "url('../img/apartment1.jpg')", 
         "price": "453321",
         "location": "Дмитриевская ул. 96/98 ",  
-        "district": "Шевченковский р-он",
+        "district": "Шевченковский",
         "code": "123", 
         "room":"1", 
         "square": "123",  
         "floor":"1",
-        }
-       // { 
-      	// "photo": "url(../img/apartment1.jpg)", 
-       //  "price": "123",
-       //  "location": "Дмитриевская ул. 96/98 ",  
-       //  "district": "Шевченковский р-он",
-       //  "code": "123", 
-       //  "room":"1", 
-       //  "square": "123",  
-       //  "floor":"1",
-       //  },
-       // { 
-      	// "photo": "url(../img/apartment1.jpg)", 
-       //  "price": "123",
-       //  "location": "Дмитриевская ул. 96/98", 
-       //  "district": "Шевченковский р-он", 
-       //  "code": "123", 
-       //  "room":"1", 
-       //  "square": "123",  
-       //  "floor":"1",
-       //  }
-];
+     	}
+	],
 
-let villas = [
+	[
+
        {
       	"photo": "url('../img/apartment1.jpg')", 
         "price": "123",
         "location": "Дмитриевская ул. 96/98 ",  
-        "district": "Шевченковский р-он",
+        "district": "Шевченковский",
         "code": "190277", 
         "room":"1", 
         "square": "123",  
         "floor":"1",
         }
-       // { 
-      	// "photo": "url(../img/apartment1.jpg)", 
-       //  "price": "123",
-       //  "location": "Дмитриевская ул. 96/98 ",  
-       //  "district": "Шевченковский р-он",
-       //  "code": "123", 
-       //  "room":"1", 
-       //  "square": "123",  
-       //  "floor":"1",
-       //  },
-       // { 
-      	// "photo": "url(../img/apartment1.jpg)", 
-       //  "price": "123",
-       //  "location": "Дмитриевская ул. 96/98", 
-       //  "district": "Шевченковский р-он", 
-       //  "code": "123", 
-       //  "room":"1", 
-       //  "square": "123",  
-       //  "floor":"1",
-       //  }
+	]
+
 ];
 
-let cards = document.querySelector('.cards');
-
-let projects = [];
-
+let checkboxHouses = document.querySelector('#checkboxHouses');
+let checkboxAparatments = document.querySelector('#checkboxAparatments');
+let checkboxVillas = document.querySelector('#checkboxVillas');
+let filter = document.querySelector('#filter');
 
 let minPrice = document.querySelector('.priceUSD--min');
 let maxPrice = document.querySelector('.priceUSD--max');
 let minPriceValue;
 let maxPriceValue;
 
-let checkboxHouses = document.querySelector('#checkboxHouses');
-let checkboxAparatments = document.querySelector('#checkboxAparatments');
-let checkboxVillas = document.querySelector('#checkboxVillas');
+function type() {
+
+let newData = [];
+let dataNext = [];
+let data3 = [];
+let data4 = [];
 
 
+let district1 = document.querySelector('#district1');
+let district2 = document.querySelector('#district2');
+let district3 = document.querySelector('#district3');
+let district4 = document.querySelector('#district4');
+let district5 = document.querySelector('#district5');
+let district6 = document.querySelector('#district6');
+let district7 = document.querySelector('#district7');
+let district8 = document.querySelector('#district8');
+let district9 = document.querySelector('#district9');
+let district10 = document.querySelector('#district10');
 
-for (let i = 0; i < houses.length; i++) {
-	houses[i].type = 'Дом';
-	projects.push(houses[i]);
-}
-for (let i = 0; i < apartments.length; i++) {
-	apartments[i].type = 'Квартира';
-	projects.push(apartments[i]);
-}
-for (let i = 0; i < villas.length; i++) {
-	villas[i].type = 'Вилла';
-	projects.push(villas[i]);
-}
+let districts = document.querySelectorAll('.district');
 
-let renderProjects = () => {
-	for (let i = 0; i < projects.length; i++) {
-		let card  = document.createElement('div');
-			card.innerHTML = `
-						<div class="card__photo photo">
-							
-						</div>
-						<div class="card__text">
-							<div class="price">
-								<span class="price--orange"> </span><br>
-							</div>
-							<div class="adress">
-								<span class="district"></span>
-								<p class="location"> Продажа в Киеве, </p>
-							</div>
-							<a href="" class="button button__details">Details</a>
-						</div>
-						<ul class="card__description">
-							<li>
-								<span>Код: </span><span class="code"></span>
-							</li>
-							<li>
-								<span>Тип: </span><span class="type"></span>
-							</li>
-							<li>
-								<span>Комната: </span><span class="room"></span>
-							</li>
-							<li>
-								<span>Площа кв.м.: </span><span class="square"></span>
-							</li>
-							<li>
-								<span>Поверх: </span><span class="floor"></span>
-							</li>
-
-						</ul>
-				`;
-
-			card.classList.add('card');
-			cards.appendChild(card);
-			let photo = document.querySelectorAll('.photo');
-			photo[i].style.backgroundImage = projects[i].photo
-
-			let price = document.querySelectorAll('.price--orange');
-			price[i].innerText = `$ ${projects[i].price} `;
-
-			let location = document.querySelectorAll('.location');
-			location[i].innerText = projects[i].location;
-
-			let district = document.querySelectorAll('.district');
-			district[i].innerHTML = ` Продажа квартиры в Киеве, <br> ${projects[i].district}`;
-
-			let code = document.querySelectorAll('.code');
-			code[i].innerText = projects[i].code;
-
-			let type = document.querySelectorAll('.type');
-			type[i].innerText = projects[i].type;
-
-			let room = document.querySelectorAll('.room');
-			room[i].innerText = projects[i].room;
-
-			let square = document.querySelectorAll('.square');
-			square[i].innerText = projects[i].square;
-
-			let floor = document.querySelectorAll('.floor');
-			floor[i].innerText = projects[i].floor;
-	}
-};
-
-renderProjects();
-
-let filterPrise = () => {
-	// при вводі оновлюємо значення
 	minPriceValue = parseInt(minPrice.value);
 	maxPriceValue = parseInt(maxPrice.value);
 
-	// Якщо поле max порожнє, робимо превірку по min
-	if (maxPrice.value === '') {
-		projects = [];
-		cards.innerHTML = '';
-		for (let i = 0; i < houses.length; i++) {
-			if(parseInt(houses[i].price) >= minPriceValue) {
-				projects.push(houses[i]);
-			}
-		}
-		renderProjects();
+
+	if(checkboxHouses.checked){
+		data[0].forEach( (elem) => {
+			dataNext.push(elem);
+		});
 	}
-	// Якщо поле min порожнє, робимо превірку по max
-	if (minPrice.value === '') {
-		projects = [];
-		cards.innerHTML = '';
-		for (let i = 0; i < houses.length; i++) {
-			if(parseInt(houses[i].price) <= maxPriceValue) {
-				projects.push(houses[i]);
-			}
-		}
-		renderProjects();
+
+	if(checkboxAparatments.checked){
+		data[1].forEach( (elem) => {
+			dataNext.push(elem);
+		});
 	}
-	// Якщо обидва порожні
-	if (maxPrice.value === '' && minPrice.value === '') {
-		projects = [];
-		cards.innerHTML = '';
-		for (let i = 0; i < houses.length; i++) {
-			projects.push(houses[i]);
-		}
-		renderProjects();
+
+	if(checkboxVillas.checked){
+		data[2].forEach( (elem) => {
+			dataNext.push(elem);
+		});
 	}
-	// Якщо обидва заповнені
+
+	if (!checkboxHouses.checked && !checkboxAparatments.checked && !checkboxVillas.checked) {
+		data.forEach( (elem) => {
+			elem.forEach( (ar) => {
+				dataNext.push(ar);
+			});
+		});
+	}
+
 	if (maxPrice.value !== '' && minPrice.value !== '') {
-		projects = [];
-		cards.innerHTML = '';
-		for (let i = 0; i < houses.length; i++) {
-			if(parseInt(houses[i].price) <= maxPriceValue && parseInt(houses[i].price) >= minPriceValue) {
-				projects.push(houses[i]);
+		for (let i = 0; i < dataNext.length; i++) {
+
+			if (dataNext[i].price >= minPriceValue &&  dataNext[i].price <= maxPriceValue) {
+				data3.push(dataNext[i]);
 			}
 		}
-		renderProjects();
 	}
-};
 
-// let filterType = () => {
+	if (maxPrice.value !== '') {
+		for (let i = 0; i < dataNext.length; i++) {
 
-	// let checkedProjects = [];
+			if (dataNext[i].price <= maxPriceValue) {
+				data3.push(dataNext[i]);
+			}
+		}
+	}
 
-// 	if(checkboxHouses.checked) {
-// 		for (let i = 0; i < houses.length; i++) {
-// 			checkedProjects.push(houses[i]);
-// 			console.log('sdf');
-// 		}
+	if (minPrice.value !== '') {
+		for (let i = 0; i < dataNext.length; i++) {
+
+			if (dataNext[i].price >= minPriceValue) {
+				data3.push(dataNext[i]);
+			}
+		}
+	}
+
+	if (maxPrice.value === '' && minPrice.value === '') {
+		for (let i = 0; i < dataNext.length; i++) {
+
+			data3.push(dataNext[i]);
+		}
+	}
+
+	if(district1.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Голосеевский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district2.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Святошинский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district3.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Соломенский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district4.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Оболонский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district5.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Подольский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district6.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Печерский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district7.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Шевченковский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district8.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Дарницкий') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district9.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Днепровский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+	if(district10.checked){
+		data3.forEach( (elem) => {
+			if (elem.district == 'Днепровский') {
+				data4.push(elem);
+			}
+		});
+
+	}
+
+	if (!district2.checked && !district3.checked && !district4.checked && !district5.checked && !district6.checked && !district7.checked && !district1.checked && !district8.checked && !district9.checked && !district10.checked ) {
+		console.log(data3);
+	} else {
+		console.log(data4);
+	}
+
+// 	for(let checke of checks){
+// 		  if(checke.checked = true){
+//   let generateCards3 = data4.map(function(name) {
+//   let subarray = [];
+//   for (let z = 0; z <Math.ceil(data4.length/size); z++){
+//     subarray[z] = data4.slice((z*size), (z*size) + size);
+// }
+//   return subarray;
+// });
+//   }
 // 	}
 
-// 	if(checkboxApartments.checked) {
-// 		for (let i = 0; i < apartments.length; i++) {
-// 			checkedProjects.push(apartments[i]);
-// 			console.log('sddddddf');
-// 		}
-// 	}
+}; 
 
-// 	if(checkboxVillas.checked) {
-// 		for (let i = 0; i < villas.length; i++) {
-// 			checkedProjects.push(villas[i]);
-// 			console.log('ssssdf');
-// 		}
-// 	}
-
-// };
-
-// let filterPrise = () => {
-// 	// при вводі оновлюємо значення
-// 	minPriceValue = parseInt(minPrice.value);
-// 	maxPriceValue = parseInt(maxPrice.value);
-
-// 	// Якщо поле max порожнє, робимо превірку по min
-// 	if (maxPrice.value === '') {
-// 		projects = [];
-// 		cards.innerHTML = '';
-// 		for (let i = 0; i < checkedProjects.length; i++) {
-// 			if(parseInt(checkedProjects[i].price) >= minPriceValue) {
-// 				projects.push(checkedProjects[i]);
-// 			}
-// 		}
-// 		renderProjects();
-// 	}
-// 	// Якщо поле min порожнє, робимо превірку по max
-// 	if (minPrice.value === '') {
-// 		projects = [];
-// 		cards.innerHTML = '';
-// 		for (let i = 0; i < checkedProjects.length; i++) {
-// 			if(parseInt(checkedProjects[i].price) <= maxPriceValue) {
-// 				projects.push(checkedProjects[i]);
-// 			}
-// 		}
-// 		renderProjects();
-// 	}
-// 	// Якщо обидва порожні
-// 	if (maxPrice.value === '' && minPrice.value === '') {
-// 		projects = [];
-// 		cards.innerHTML = '';
-// 		for (let i = 0; i < checkedProjects.length; i++) {
-// 			projects.push(checkedProjects[i]);
-// 		}
-// 		renderProjects();
-// 	}
-// 	// Якщо обидва заповнені
-// 	if (maxPrice.value !== '' && minPrice.value !== '') {
-// 		projects = [];
-// 		cards.innerHTML = '';
-// 		for (let i = 0; i < checkedProjects.length; i++) {
-// 			if(parseInt(checkedProjects[i].price) <= maxPriceValue && parseInt(checkedProjects[i].price) >= minPriceValue) {
-// 				projects.push(checkedProjects[i]);
-// 			}
-// 		}
-// 		renderProjects();
-// 	}
-// };
-
-let filter = () => {
-	filterPrise();
-};
-
-minPrice.addEventListener('input', function(){
-	filter();
+filter.addEventListener('click', function() {
+	type();
 });
 
 
-maxPrice.addEventListener('input', function(){
-	filter();
-});
+let checks = document.querySelectorAll('.check');
+
+for(let check of checks){
+	check.addEventListener('click', function(){
+		type();
+	})
+}
 
 
 
 
-
-// checkboxHouses.addEventListener('change', function() {
-
+// let generateCards5 = data4.map(function(name) {
+//   return name.length;
 // });
 
-
-
-
+// let generateCards10 = data4.map(function(name) {
+//   return name.length;
+// });
