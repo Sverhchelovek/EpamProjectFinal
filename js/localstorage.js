e0246835223a9597f16324b4d35859564e74694b
Bogdan Shon <bogdan5321@gmail.com>
2019-08-07 16:18:22 +0300

    prefixes css added

--- js/localstorage.js
+++
@@ -1,3 +0,0 @@
-var a = [];
-a.push(JSON.parse(localStorage.getItem('session')));
-localStorage.setItem('session', JSON.stringify(a));
--- js/popup.js
+++ js/popup.js
@@ -2,16 +2,12 @@ let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.btn__close');
 let contactButtons  = document.querySelectorAll('.contact__button');
 
-
-
-
-
-	for (let contactButton of contactButtons) {
-		contactButton.addEventListener('click', function(){
+for (let contactButton of contactButtons) {
+	contactButton.addEventListener('click', function(){
 	popup.classList.add('popup--active');
 });
 
 popupClose.addEventListener('click', function(){
 	popup.classList.remove('popup--active');
-});
-			}
+	});
+}
